242fccd153d1bdd327f428e34b63dcf57987d7cb
Lordofshadow928 <flairdante@gmail.com>
2025-08-25 17:47:31 +0700

    Add project files.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float moveSpeed = 10f;
+
+    private void CanMove()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+        Vector3 direction = transform.right * hInput + transform.forward * vInput;
+        controller.SimpleMove(direction * moveSpeed);
+    }
+
+    private void Start()
+    {
+        
+    }
+}
