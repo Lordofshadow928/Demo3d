a82d215b311bbaef4cf2a2416fc183905078a38d
Lordofshadow928 <flairdante@gmail.com>
2025-09-15 19:19:40 +0700

    add ragdoll switcher

---
+++ Assets/Scripts/RagdollSwitcher.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollSwitcher : MonoBehaviour
+{
+    public Animator anim;
+    public Rigidbody[] rigids;
+
+    [ContextMenu("Retrieve Rigidbodies")]
+    private void RetrieveRigidbodies()
+    {
+        rigids = GetComponentsInChildren<Rigidbody>();
+    }
+
+    [ContextMenu("Clear Ragdoll")] 
+    private void ClearRagdoll()
+    {
+        CharacterJoint[] joints = GetComponentsInChildren<CharacterJoint>();
+        for (int i = 0; i < joints.Length; i++)
+        {
+            DestroyImmediate(joints[i]);
+        }
+
+        Rigidbody[] rigidList = GetComponentsInChildren<Rigidbody>();
+        for (int i = 0; i < rigidList.Length; i++)
+        {
+            DestroyImmediate(rigidList[i]);
+        }
+
+        Collider[] colls = GetComponentsInChildren<Collider>();
+        for(int i = 0; i < colls.Length; i++)
+        {
+            DestroyImmediate(colls[i]);
+        }
+    }
+
+    [ContextMenu("Enable Ragdoll")]
+
+    private void EnableRagdoll()
+    {
+        SetRagDoll(true);
+    }
+
+    [ContextMenu("Disable Ragdoll")]
+
+    private void DisableRagdoll()
+    {
+        SetRagDoll(false);
+    }
+    private void SetRagDoll(bool ragdollEnable)
+    {
+        anim.enabled = !ragdollEnable;
+        for (int i = 0; i < rigids.Length; i++)
+        {
+            rigids[i].isKinematic = !ragdollEnable;
+        }
+
+    }
+}
