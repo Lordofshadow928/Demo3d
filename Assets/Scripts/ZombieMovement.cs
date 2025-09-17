a679c25f0948206b63544ed032eb8a01ef2c70bd
Lordofshadow928 <flairdante@gmail.com>
2025-09-17 20:44:29 +0700

    zombie movement, zombie navmesh

--- Assets/Scripts/ZombieMovement.cs
+++ Assets/Scripts/ZombieMovement.cs
@@ -1,9 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ZombieMovement : MonoBehaviour
 {
     public Transform playerFoot;
-    
+    public Animator anim;
+    public NavMeshAgent agent;
+    public float reachingRadius;
+
+    private void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, playerFoot.position);
+        if (distanceToPlayer > reachingRadius)
+        {
+            anim.SetBool("isWalk", true);
+            agent.isStopped = false;
+            agent.SetDestination(playerFoot.position);
+        }
+        else
+        {
+            anim.SetBool("isWalk", false);
+            agent.isStopped = true;
+        }
+    }
+
 }
